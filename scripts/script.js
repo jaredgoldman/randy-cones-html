d4c920516f4b72c0cc4b0f2684a7476b3f2db447
jaredgoldman <jaredgoldmandrums@gmail.com>
2023-01-21 19:50:16 -0500

    initial commit

---
+++ scripts/script.js
@@ -0,0 +1,24 @@
+const appendRepeatedText = (text, symbol, element, length = 20) => {
+  const repeatText = `${text} ${symbol} `;
+  const repeatedArray = Array(length).fill(repeatText);
+  repeatedArray.forEach((text) => {
+    const textNode = document.createElement('span');
+    textNode.textContent = text;
+    element.appendChild(textNode);
+  });
+};
+
+window.addEventListener('load', () => {
+  const textArr = [
+    'SERIES 4/5 SOON',
+    'ALL NEW TRAITS',
+    'RANDY PIXELS SOLD OUT',
+    'META-DESSERT',
+    'SCOOP',
+  ];
+
+  textArr.forEach((text, i) => {
+    const el = document.querySelector(`.divider-${i + 1}`);
+    appendRepeatedText(text, '-', el);
+  });
+});
