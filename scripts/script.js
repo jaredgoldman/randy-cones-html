f1be18ce692a5a408aa96768f8e243b8d399a646
nathandellvandenberg <dell.vandenberg@gmail.com>
2023-01-22 21:34:54 -0500

    added Randy Cones Series 5 GIF and changed banner texts

--- scripts/script.js
+++ scripts/script.js
@@ -2,23 +2,23 @@ const appendRepeatedText = (text, symbol, element, length = 20) => {
   const repeatText = `${text} ${symbol} `;
   const repeatedArray = Array(length).fill(repeatText);
   repeatedArray.forEach((text) => {
-    const textNode = document.createElement('span');
+    const textNode = document.createElement("span");
     textNode.textContent = text;
     element.appendChild(textNode);
   });
 };
 
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
   const textArr = [
-    'SERIES 4/5 SOON',
-    'ALL NEW TRAITS',
-    'RANDY PIXELS SOLD OUT',
-    'META-DESSERT',
-    'SCOOP',
+    "SERIES 5/5 SOON",
+    "ALL TRAITS",
+    "RANDY PIXELS SOLD OUT",
+    "META-DESSERT",
+    "SCOOP",
   ];
 
   textArr.forEach((text, i) => {
     const el = document.querySelector(`.divider-${i + 1}`);
-    appendRepeatedText(text, '-', el);
+    appendRepeatedText(text, "-", el);
   });
 });
